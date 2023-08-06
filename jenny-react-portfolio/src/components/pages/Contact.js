507c0d7d15496a2a6042096ced9c76ad252fcd91
jnaoroji <jennynaoroji@gmail.com>
2023-08-06 23:28:06 +1000

    added styling to contact form

--- jenny-react-portfolio/src/components/pages/Contact.js
+++ jenny-react-portfolio/src/components/pages/Contact.js
@@ -59,8 +59,8 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
-      <section className="page-section" id="contact">
-        <div className="container px-4 px-lg-5">
+      <section className="page-section bg-primary" id="contact">
+        <div className="container px-4 py-4 px-lg-5 py-lg-5 bg-white rounded">
           <div className="row gx-4 gx-lg-5 justify-content-center">
             <div className="col-lg-8 col-xl-6 text-center">
               <h2 className="mt-0">Let's Get In Touch!</h2>
@@ -113,12 +113,7 @@ export default function Contact() {
               </form>
             </div>
           </div>
-                <div className="row gx-4 gx-lg-5 justify-content-center">
-                    <div className="col-lg-4 text-center mb-5 mb-lg-0">
-                        <i className="bi-phone fs-2 mb-3 text-muted"></i>
-                        <div>+1 (555) 123-4567</div>
-                    </div>
-                </div>
+                
             </div>
         </section>
     </div>
