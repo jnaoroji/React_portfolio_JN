b31bb0e0993c70e55f38016e387e7990b3010fc5
jnaoroji <jennynaoroji@gmail.com>
2023-08-06 19:22:20 +1000

    added a width and height to the potfolio sections

--- jenny-react-portfolio/src/components/pages/Portfolio.js
+++ jenny-react-portfolio/src/components/pages/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import background from "../../assets/img/portfolio/thumbnails/1.jpg";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const styles = {
+// const style = {
 //   backgroundImage: {
 //     backgroundImage: `url("../../assets/img/portfolio/thumbnails/1")`,
 //     backgroundSize: 'cover',
@@ -13,49 +13,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //   },
 // };
 
+
 export default function Portfolio() {
   return (
     <div>
       <section className="page-section bg-primary" id="portfolio">
         {/* <div className="container-fluid p-0"> */}
           <div className="row g-0">
-          <div className="col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover" }}>
+          <div className="portfolio-box col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", height: '300px',width: '33.33%', }}>
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Project</div>
                                 <div className="project-name">Project Name</div>
                             </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover" }}>
-                            {/* <img className="img-fluid" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover" }} alt="thumbnail1" /> */}
+          <div className="portfolio-box col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", height: '300px',width: '33.33%', }}>
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Project</div>
                                 <div className="project-name">Project Name</div>
                             </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover" }}>
+          <div className="portfolio-box col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", height: '300px',width: '33.33%', }}>
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Project</div>
                                 <div className="project-name">Project Name</div>
                             </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover" }}>
+          <div className="portfolio-box col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", height: '300px',width: '33.33%', }}>
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Project</div>
                                 <div className="project-name">Project Name</div>
                             </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover" }}>
+          <div className="portfolio-box col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", height: '300px',width: '33.33%', }}>
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Project</div>
                                 <div className="project-name">Project Name</div>
                             </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover" }}>
+          <div className="portfolio-box col-lg-3 col-md-4 col-sm-6" style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", height: '300px',width: '33.33%', }}>
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Project</div>
                                 <div className="project-name">Project Name</div>
                             </div>
           </div>
+
         </div>
       </section>
     </div>
