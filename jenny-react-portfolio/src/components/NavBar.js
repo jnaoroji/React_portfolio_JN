1aeeff6ade94e918c0d4a5945ed047e35f4214d5
jnaoroji <jennynaoroji@gmail.com>
2023-08-07 14:40:09 +1000

    trying to make the onclick function for menu toggle work

--- jenny-react-portfolio/src/components/NavBar.js
+++ jenny-react-portfolio/src/components/NavBar.js
@@ -1,9 +1,18 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React from 'react';
+import React, { useState } from 'react';
+
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavBar({ currentPage, handlePageChange }) {
+  // Create state variable to track menu state
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu state
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
       <div className="container px-4 px-lg-5">
@@ -15,7 +24,17 @@ function NavBar({ currentPage, handlePageChange }) {
           >
           <div className="navbar-brand">Jenny Naoroji</div>
         </a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+        <button 
+        className="navbar-toggler navbar-toggler-right" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarResponsive" 
+        aria-controls="navbarResponsive" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation"
+        onClick={() => handleMenuToggle()} // attach the function to onClick event
+        ><span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="nav nav-tabs navbar-nav ms-auto my-2 my-lg-0">
             <li className="nav-item">
