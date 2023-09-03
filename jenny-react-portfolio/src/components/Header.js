8064a27ef88cfdade6eaf292e3f5f4767814577a
jnaoroji <jennynaoroji@gmail.com>
2023-09-03 14:58:18 +1000

    changed home page to home

--- jenny-react-portfolio/src/components/Header.js
+++ jenny-react-portfolio/src/components/Header.js
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 
 
 export default function Header() {
-  const [currentPage, setCurrentPage] = useState('Portfolio');
+  const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
